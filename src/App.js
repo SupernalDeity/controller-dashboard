bc4feccab8b1ddb4f46d7d439402ad650bdada23
Anthony <perezant121@gmail.com>
2022-12-05 02:30:20 -0500

    contact form setup

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ import Sidebar from "./scenes/global/Sidebar";
 import Team from "./scenes/team";
 import Contacts from "./scenes/contacts";
 import Invoices from "./scenes/invoices";
+import Form from "./scenes/form";
 // import Calender from "./scenes/calender";
 // import Bar from "./scenes/bar";
-// import Form from "./scenes/form";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
 // import FAQ from "./scenes/faq";
@@ -33,13 +33,13 @@ function App() {
               <Route path='/team' element={<Team />} />
               <Route path='/contacts' element={<Contacts />} />
               <Route path='/invoices' element={<Invoices />} />
-              {/* <Route path='/form' element={<Form />} /> */}
+              <Route path='/form' element={<Form />} />
+              {/* <Route path='/calender' element={<Calender />} /> */}
               {/* <Route path='/bar' element={<Bar />} /> */}
               {/* <Route path='/pie' element={<Pie />} /> */}
               {/* <Route path='/line' element={<Line />} /> */}
               {/* <Route path='/faq' element={<FAQ />} /> */}
               {/* <Route path='/geography' element={<Geography />} /> */}
-              {/* <Route path='/calender' element={<Calender />} /> */}
             </Routes>
           </main>
         </div>
