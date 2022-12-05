a5de13f415c336ffa2eadadf54f892178a891279
Anthony <perezant121@gmail.com>
2022-12-05 03:57:06 -0500

    geography chart created

--- src/components/GeographyChart.jsx
+++ src/components/GeographyChart.jsx
@@ -82,5 +82,5 @@ const GeographyChart = ({ isDashboard = false }) => {
     />
   );
 };
-
+ 
 export default GeographyChart;
--- src/scenes/geography/index.jsx
+++ src/scenes/geography/index.jsx
@@ -7,7 +7,7 @@ import { useTheme } from '@mui/material';
 const Geography = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
-  
+   
   return (
     <Box m='20px'>
       <Header title='Geography Chart' subtitle='Simple Geography Chart' />
