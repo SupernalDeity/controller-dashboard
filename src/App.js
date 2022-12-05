3926916fa8db67e2f3e32efb00ed5bf5869fcd61
Anthony <perezant121@gmail.com>
2022-12-04 20:25:04 -0500

    sidebar organized and setup

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
-import { ColorModeContext, useMode } from "./theme";
+import { useState } from 'react';
+import { ColorModeContext, useMode } from './theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
 import Topbar from './scenes/global/Topbar';
 import Dashboard from "./scenes/dashboard";
 import Sidebar from "./scenes/global/Sidebar";
-import Calender from "./scenes/calender";
-import Team from "./scenes/team";
-import Invoices from "./scenes/invoices";
-import Contacts from "./scenes/contacts";
-import Bar from "./scenes/bar";
-import Form from "./scenes/form";
-import Line from "./scenes/line";
-import Pie from "./scenes/pie";
-import FAQ from "./scenes/faq";
-import Geography from "./scenes/geography";
+// import Calender from "./scenes/calender";
+// import Team from "./scenes/team";
+// import Invoices from "./scenes/invoices";
+// import Contacts from "./scenes/contacts";
+// import Bar from "./scenes/bar";
+// import Form from "./scenes/form";
+// import Line from "./scenes/line";
+// import Pie from "./scenes/pie";
+// import FAQ from "./scenes/faq";
+// import Geography from "./scenes/geography";
 
 function App() {
   const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
 
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <div className="App">
+        <div className="app">
+          <Sidebar isSidebar={isSidebar} />
           <main className="content">
-            <Topbar />
+            <Topbar setIsSidebar={setIsSidebar}/>
             <Routes>
               <Route path='/' element={<Dashboard />} />
               {/* <Route path='/team' element={<Team />} /> */}
@@ -40,7 +44,7 @@ function App() {
           </main>
         </div>
       </ThemeProvider>
-    </ ColorModeContext.Provider>
+    </ColorModeContext.Provider>
   );
 }
 
--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,6 +1,13 @@
+import Header from "../../components/Header";
+import { Box } from '@mui/material';
+
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <Box m='20px'>
+      <Box display='flex' justifyContent='space-between' alignItems='center'>
+        <Header title='DASHBOARD' subtitle='Welcome to your dashboard' />
+      </Box>
+    </Box>
   )
 }
 
